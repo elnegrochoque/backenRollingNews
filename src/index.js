16a7d229b6bc4e0bf3b4b2d8e914f67fe32ea343
Matias <elnegrochoque@gmail.com>
2021-05-26 22:56:36 -0300

    se agrego la configuracion inicial del proyecto

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
+
+const app = express();
+
+const port = 4000;
+
+app.set('port', process.env.PORT || 4000);
+
+app.listen((app.get('port')), () => {
+    
+    console.log('estoy en el puerto ' + app.get('port'))
+}
+);
+
+// Middlewares
+app.use(morgan('dev'));
+//cors nos permite realizar las peticiones al backend
+app.use(cors());
+// de express
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// agrego la carpeta public como estatica
+
+app.use(express.static(path.join(__dirname,'../public')));
+
+
+
+app.get('/',(req,res)=>{
+    res.send('hola  desde el servidor');
+})
+
